d05c676bbe5f62f73d209a4ec89e498d43a11509
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2023-10-21 13:51:51 +1000

    Update dynamic list

--- Sources/SwiftUIComponents/Components/Containers/DynamicList.swift
+++ Sources/SwiftUIComponents/Components/Containers/DynamicList.swift
@@ -1,55 +1,120 @@
+import Foundation
 import SwiftUI
 
-public struct DynamicList<Content: View>: View {
-    public enum Orientation {
-        case horizontal
-        case vertical
-    }
+public enum Orientation {
+    case horizontal
+    case vertical
+}
 
+public struct StackView<Content: View>: View {
+    private var orientation: Orientation
+    private let content: Content
+    
+    init(orientation: Orientation, @ViewBuilder content: () -> Content) {
+        self.orientation = orientation
+        self.content = content()
+    }
+    
+    public var body: some View {
+        if orientation == .horizontal {
+            HStack(spacing: 0) {
+                content
+            }
+        } else {
+            VStack(spacing: 0) {
+                content
+            }
+        }
+    }
+}
+
+public struct DynamicList<Content: View>: View {
     private class ViewModel: ObservableObject {
         @Published var scrollOffset: Double = 0
         var previousScrollOffset: Double = 0
     }
     
     @StateObject private var viewModel = ViewModel()
+    @Binding private var scrollOffset: Double
+    
+    private let animation: Animation = .easeInOut(duration: 0.3)
     
     private let lengths: [Double]
+    private let length: Double
     private let cellBuilder: (Int) -> Content
     private var orientation: Orientation
 
-    public init(orientation: Orientation = .horizontal, numberOfItems: Int, itemLength: Double, viewForCell cellBuilder: @escaping (Int) -> Content) {
+    public init(
+        scrollOffset: Binding<Double>,
+        orientation: Orientation = .horizontal,
+        numberOfItems: Int,
+        itemLength: Double,
+        viewForCell cellBuilder: @escaping (Int) -> Content
+    ) {
         self.lengths = Array(repeating: itemLength, count: numberOfItems)
+        self.length = itemLength * Double(numberOfItems)
         self.cellBuilder = cellBuilder
         self.orientation = orientation
+        self._scrollOffset = scrollOffset
     }
     
-    public init(orientation: Orientation = .horizontal, itemLengths: [Double], viewForCell cellBuilder: @escaping (Int) -> Content) {
+    public init(
+        scrollOffset: Binding<Double>,
+        orientation: Orientation = .horizontal,
+        itemLengths: [Double],
+        viewForCell cellBuilder: @escaping (Int) -> Content
+    ) {
         self.lengths = itemLengths
+        self.length = itemLengths.reduce(0.0, +)
         self.cellBuilder = cellBuilder
         self.orientation = orientation
+        self._scrollOffset = scrollOffset
     }
-
     
     public var body: some View {
         GeometryReader { geometry in
             listView(geometry.size)
+                .onChange(of: scrollOffset) { value in
+                    guard scrollOffset != viewModel.scrollOffset else { return }
+                    
+                    let scrollOffset: Double
+                    switch orientation {
+                    case .horizontal:
+                        scrollOffset = max(min(0, value), geometry.size.width - length)
+                    case .vertical:
+                        scrollOffset = max(min(0, value), geometry.size.height - length)
+                    }
+                    
+                    viewModel.previousScrollOffset = scrollOffset
+                    if self.scrollOffset != scrollOffset {
+                        self.scrollOffset = scrollOffset
+                    }
+                    
+                    withAnimation(animation) {
+                        viewModel.scrollOffset = scrollOffset
+                    }
+                }
         }
     }
     
     private var previousScrollOffset: Double {
         viewModel.previousScrollOffset
     }
-    
-    private var dragGesture: some Gesture {
+
+    private func dragGesture(screenDimension size: Double) -> some Gesture {
         DragGesture()
             .onChanged{ value in
                 let translation = orientation == .horizontal ? value.translation.width : value.translation.height
-                viewModel.scrollOffset = min(0, previousScrollOffset + translation)
+                let scrollOffset = max(min(0, previousScrollOffset + translation), size - length)
+                viewModel.scrollOffset = scrollOffset
+                self.scrollOffset = scrollOffset
             }
             .onEnded { value in
-                withAnimation {
+                withAnimation(animation) {
                     let translation = orientation == .horizontal ? value.translation.width : value.translation.height
-                    viewModel.scrollOffset = min(0, previousScrollOffset + translation)
+                    let scrollOffset = max(min(0, previousScrollOffset + translation), size - length)
+                    viewModel.scrollOffset = scrollOffset
+                    self.scrollOffset = scrollOffset
                     viewModel.previousScrollOffset = viewModel.scrollOffset
                 }
             }
@@ -105,19 +170,19 @@ public struct DynamicList<Content: View>: View {
         let endIndex = min(endIndex(for: start, screenWidth: screenDimension) + 1, numberOfItems)
         let visibleRange = start.index ..< endIndex
         
-        let leadingPadding: Double
+        let padding: Double
         switch orientation {
         case .horizontal:
-            leadingPadding = max(0, start.width - lengths[start.index])
+            padding = max(0, start.width - lengths[start.index])
         case .vertical:
-            leadingPadding = max(0, start.width - lengths[start.index])
+            padding = max(0, start.width - lengths[start.index])
         }
         
-        return HStack(spacing: 0) {
+        return StackView(orientation: orientation) {
             Spacer()
                 .frame(
-                    width: orientation == .horizontal ? leadingPadding : nil,
-                    height: orientation == .vertical ? leadingPadding : nil
+                    width: orientation == .horizontal ? padding : nil,
+                    height: orientation == .vertical ? padding : nil
                 )
             ForEach(visibleRange, id: \.hashValue) { index in
                 cellBuilder(index)
@@ -125,13 +190,20 @@ public struct DynamicList<Content: View>: View {
                         width: orientation == .horizontal ? lengths[index] : nil,
                         height: orientation == .vertical ? lengths[index] : nil
                     )
+                    .transition(
+                        .offset(
+                            x: orientation == .horizontal ? index > (endIndex - start.index) ? lengths[start.index] : -lengths[start.index] : 0,
+                            y: orientation == .vertical ? index > (endIndex - start.index) ? lengths[start.index] : -lengths[start.index] : 0
+                        )
+                    )
+                    .background(Color.blue)
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .offset(x: orientation == .horizontal ? viewModel.scrollOffset : 0,
                 y: orientation == .vertical ? viewModel.scrollOffset : 0)
         .contentShape(Rectangle())
-        .gesture(dragGesture)
+        .gesture(dragGesture(screenDimension: screenDimension))
     }
     
     public func orientation(_ orientation: Orientation) -> Self {
@@ -141,3 +213,34 @@ public struct DynamicList<Content: View>: View {
     }
 }
 
+// MARK: - Public Init
+
+extension DynamicList {
+    public init(
+        orientation: Orientation = .horizontal,
+        numberOfItems: Int,
+        itemLength: Double,
+        viewForCell cellBuilder: @escaping (Int) -> Content
+    ) {
+        self.init(
+            scrollOffset: .constant(0),
+            orientation: orientation,
+            numberOfItems: numberOfItems,
+            itemLength: itemLength,
+            viewForCell: cellBuilder
+        )
+    }
+    
+    public init(
+        orientation: Orientation = .horizontal,
+        itemLengths: [Double],
+        viewForCell cellBuilder: @escaping (Int) -> Content
+    ) {
+        self.init(
+            scrollOffset: .constant(0),
+            orientation: orientation,
+            itemLengths: itemLengths,
+            viewForCell: cellBuilder
+        )
+    }
+}
