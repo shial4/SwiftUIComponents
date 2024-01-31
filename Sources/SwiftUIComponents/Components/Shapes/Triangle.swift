e604f1c972cd788f5683088bc744e19dfa18b4f4
Szymon Lorenz <szymon.lorenz.solutions@gmail.com>
2024-01-31 19:29:07 +1000

    Add new shapes

---
+++ Sources/SwiftUIComponents/Components/Shapes/Triangle.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct Triangle: Shape {
+    enum Orientation {
+        case leading, trailing, top, bottom
+    }
+
+    let orientation: Orientation
+
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        if orientation == .leading {
+            path.move(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+        } else if orientation == .trailing {
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.midY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        } else if orientation == .top {
+            path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        } else {
+            path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.midX, y: rect.maxY))
+            path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+            path.addLine(to: CGPoint(x: rect.minX, y: rect.minY))
+        }
+
+        return path
+    }
+}
---
+++ Sources/SwiftUIComponents/Components/Shapes/XMark.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+struct XMark: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.minY))
+
+        return path
+    }
+}
+
+#Preview {
+    XMark()
+}
